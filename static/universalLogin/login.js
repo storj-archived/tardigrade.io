e37c08bf4a39c3db37e809a9d4ee635aba73141f
VitaliiShpital <takur12345@gmail.com>
2019-11-26 19:57:56 +0200

    js refactored

--- static/universalLogin/login.js
+++ static/universalLogin/login.js
@@ -1,26 +1,9 @@
 // Copyright (C) 2019 Storj Labs, Inc.
 // See LICENSE for copying information.
 
-function setIframeSource(url) {
-    document.getElementById('iframe').src = url;
-}
-
 function selectUSSatellite() {
-    document.getElementById('europe-container').classList.remove('selected-container');
-    document.getElementById('europe-container').classList.add('satellite-container');
-    document.getElementById('europe-svg-path').classList.remove('selected-image');
-    document.getElementById('europe-title').classList.remove('selected-title');
-    document.getElementById('europe-subtitle').classList.remove('selected-subtitle');
-    document.getElementById('europe-checkmark').classList.remove('selected-checkmark');
-    document.getElementById('europe-checkmark').classList.add('checkmark');
-
-    document.getElementById('asia-container').classList.remove('selected-container');
-    document.getElementById('asia-container').classList.add('satellite-container');
-    document.getElementById('asia-svg-path').classList.remove('selected-image');
-    document.getElementById('asia-title').classList.remove('selected-title');
-    document.getElementById('asia-subtitle').classList.remove('selected-subtitle');
-    document.getElementById('asia-checkmark').classList.remove('selected-checkmark');
-    document.getElementById('asia-checkmark').classList.add('checkmark');
+    unselectEuropeContainer();
+    unselectAsiaContainer();
 
     document.getElementById('us-container').classList.remove('satellite-container');
     document.getElementById('us-container').classList.add('selected-container');
@@ -34,24 +17,8 @@ function selectUSSatellite() {
 }
 
 function selectAsiaSatellite() {
-    document.getElementById('europe-container').classList.remove('selected-container');
-    document.getElementById('europe-container').classList.add('satellite-container');
-    document.getElementById('europe-svg-path').classList.remove('selected-image');
-    document.getElementById('europe-title').classList.remove('selected-title');
-    document.getElementById('europe-subtitle').classList.remove('selected-subtitle');
-    document.getElementById('europe-checkmark').classList.remove('selected-checkmark');
-    document.getElementById('europe-checkmark').classList.add('checkmark');
-
-    document.getElementById('us-container').classList.remove('selected-container');
-    document.getElementById('us-container').classList.add('satellite-container');
-    document.getElementById('us-svg-path').classList.remove('selected-image');
-    document.getElementById('us-svg-path').classList.add('us-svg-path');
-    document.getElementById('us-title').classList.remove('selected-title');
-    document.getElementById('us-title').classList.add('satellite-name');
-    document.getElementById('us-subtitle').classList.remove('selected-subtitle');
-    document.getElementById('us-subtitle').classList.add('storj-logo-text');
-    document.getElementById('us-checkmark').classList.remove('selected-checkmark');
-    document.getElementById('us-checkmark').classList.add('checkmark');
+    unselectEuropeContainer();
+    unselectUSContainer();
 
     document.getElementById('asia-container').classList.remove('satellite-container');
     document.getElementById('asia-container').classList.add('selected-container');
@@ -65,24 +32,8 @@ function selectAsiaSatellite() {
 }
 
 function selectEuropeSatellite() {
-    document.getElementById('us-container').classList.remove('selected-container');
-    document.getElementById('us-container').classList.add('satellite-container');
-    document.getElementById('us-svg-path').classList.remove('selected-image');
-    document.getElementById('us-svg-path').classList.add('us-svg-path');
-    document.getElementById('us-title').classList.remove('selected-title');
-    document.getElementById('us-title').classList.add('satellite-name');
-    document.getElementById('us-subtitle').classList.remove('selected-subtitle');
-    document.getElementById('us-subtitle').classList.add('storj-logo-text');
-    document.getElementById('us-checkmark').classList.remove('selected-checkmark');
-    document.getElementById('us-checkmark').classList.add('checkmark');
-
-    document.getElementById('asia-container').classList.remove('selected-container');
-    document.getElementById('asia-container').classList.add('satellite-container');
-    document.getElementById('asia-svg-path').classList.remove('selected-image');
-    document.getElementById('asia-title').classList.remove('selected-title');
-    document.getElementById('asia-subtitle').classList.remove('selected-subtitle');
-    document.getElementById('asia-checkmark').classList.remove('selected-checkmark');
-    document.getElementById('asia-checkmark').classList.add('checkmark');
+    unselectUSContainer();
+    unselectAsiaContainer();
 
     document.getElementById('europe-container').classList.remove('satellite-container');
     document.getElementById('europe-container').classList.add('selected-container');
@@ -93,4 +44,41 @@ function selectEuropeSatellite() {
     document.getElementById('europe-checkmark').classList.add('selected-checkmark');
 
     setIframeSource('https://europe-west-1.tardigrade.io/login');
+}
+
+function unselectEuropeContainer() {
+    document.getElementById('europe-container').classList.remove('selected-container');
+    document.getElementById('europe-container').classList.add('satellite-container');
+    document.getElementById('europe-svg-path').classList.remove('selected-image');
+    document.getElementById('europe-title').classList.remove('selected-title');
+    document.getElementById('europe-subtitle').classList.remove('selected-subtitle');
+    document.getElementById('europe-checkmark').classList.remove('selected-checkmark');
+    document.getElementById('europe-checkmark').classList.add('checkmark');
+}
+
+function unselectAsiaContainer() {
+    document.getElementById('asia-container').classList.remove('selected-container');
+    document.getElementById('asia-container').classList.add('satellite-container');
+    document.getElementById('asia-svg-path').classList.remove('selected-image');
+    document.getElementById('asia-title').classList.remove('selected-title');
+    document.getElementById('asia-subtitle').classList.remove('selected-subtitle');
+    document.getElementById('asia-checkmark').classList.remove('selected-checkmark');
+    document.getElementById('asia-checkmark').classList.add('checkmark');
+}
+
+function unselectUSContainer() {
+    document.getElementById('us-container').classList.remove('selected-container');
+    document.getElementById('us-container').classList.add('satellite-container');
+    document.getElementById('us-svg-path').classList.remove('selected-image');
+    document.getElementById('us-svg-path').classList.add('us-svg-path');
+    document.getElementById('us-title').classList.remove('selected-title');
+    document.getElementById('us-title').classList.add('satellite-name');
+    document.getElementById('us-subtitle').classList.remove('selected-subtitle');
+    document.getElementById('us-subtitle').classList.add('storj-logo-text');
+    document.getElementById('us-checkmark').classList.remove('selected-checkmark');
+    document.getElementById('us-checkmark').classList.add('checkmark');
+}
+
+function setIframeSource(url) {
+    document.getElementById('iframe').src = url;
 }
