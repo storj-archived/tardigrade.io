cc716cd8c3470ef53f9b88a3d0a6808f4adee126
Tome Boshevski <tome@boshevski.com>
2020-12-16 11:34:33 +0100

    Add loading spinner animation to the fastly form submission

--- static/js/fastly-form.js
+++ static/js/fastly-form.js
@@ -1,8 +1,10 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbwiDUmz3MDY_fqjobWAWdm_H0XZ1jyyCsTCddQiVmYKgMTv8z4/exec'
 const form = document.forms['submit-to-google-sheet']
+const spinner = document.getElementById("spinner");
 
 form.addEventListener('submit', e => {
   e.preventDefault()
+  spinner.removeAttribute('hidden');
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
     .then(response => window.location.href = "/fastly-success")
     .catch(error => console.error('Error!', error.message))
